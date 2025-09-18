ae61f16073b43395c52ccfdab7751243454f2584
rajnish2025 <rajnish@veriqos.com>
2025-09-18 17:24:36 +0530

    email subscription added.

--- src/pages/DocSubscribe.jsx
+++ src/pages/DocSubscribe.jsx
@@ -1,59 +1,78 @@
-import React, { useRef } from 'react'
-import { useState, useEffect } from 'react';
+import React, { useRef } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useForm, ValidationError } from "@formspree/react";
 
 const DocSubscribe = () => {
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const location  = useLocation();
-    const emailRef = useRef();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const location = useLocation();
+  const emailRef = useRef();
+  const [state, handleSubmit] = useForm("mandggpl");
 
-    useEffect(() => {
+  useEffect(() => {
     setIsDialogOpen(true);
-    }, [location.pathname]);
-    const handleSubscribe = () =>{
-        const email = emailRef.current.value;
-        if(email){
-            localStorage.setItem('email', email);
-            setIsDialogOpen(false);
-            window.location.reload();
-        } 
-    }   
-    return (
+  }, [location.pathname]);
+
+  if (state.succeeded) {
+    console.log(state, emailRef.current.value);
+    localStorage.setItem("email", emailRef.current.value);
+    setIsDialogOpen(false);
+    window.location.reload();
+  }
+
+  return (
     <div>
-        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        
-            <DialogContent className="sm:max-w-[550px] py-10">
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent className="sm:max-w-[550px] py-10">
+          <form onSubmit={handleSubmit}>
             <DialogHeader>
-                <DialogTitle>Subscribe To Docs</DialogTitle>
-                <DialogDescription>
-                Write your email here. Click subscribe when you&apos;re
-                done.
-                </DialogDescription>
+              <DialogTitle>Subscribe To Docs</DialogTitle>
+              <DialogDescription>
+                Write your email here. Click subscribe when you&apos;re done.
+              </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4">
-                <div className="grid gap-3">
+              <div className="grid gap-3">
                 <Label htmlFor="email-1">Email</Label>
-                <Input id="email-1" name="email" placeholder="example@gmail.com" ref={emailRef}/>
-                </div>
+                <Input
+                  id="email-1"
+                  name="email"
+                  type="email"
+                  placeholder="example@gmail.com"
+                  ref={emailRef}
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
+              </div>
             </div>
-            <DialogFooter>
-                <DialogClose asChild>
+            <DialogFooter className="mt-5">
+              <DialogClose asChild>
                 <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button onClick={handleSubscribe}>Subscribe</Button>
+              </DialogClose>
+              <Button type="submit" disabled={state.submitting}>
+                Subscribe
+              </Button>
             </DialogFooter>
-            </DialogContent>
-        </Dialog>
+          </form>
+        </DialogContent>
+      </Dialog>
     </div>
-    );
-}
+  );
+};
 
 export default DocSubscribe;
-
-
-
