527811e9323235c7c51ef56db6e3ee3c04136559
rajnish2025 <rajnish@veriqos.com>
2025-09-18 18:18:24 +0530

    fixed navigation

--- src/utilities/ProtectedRoutes.jsx
+++ src/utilities/ProtectedRoutes.jsx
@@ -4,7 +4,9 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoutes = ({ children }) => {
   let token = localStorage.getItem("token");
   console.log("temp token: ", typeof import.meta.env.VITE_TEM_TOKEN, token);
-  if (!token || token != import.meta.env.VITE_TEM_TOKEN) {
+  if (!token) {
+    return <Navigate to="/docs" replace />;
+  } else if (token != import.meta.env.VITE_TEM_TOKEN) {
     return <Navigate to="/docs" replace />;
   }
   return children;
